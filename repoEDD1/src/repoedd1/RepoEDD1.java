54409bc9dd1f0283b2f28d20faef55522092e4a5
Cèlia <Clia@Celia>
2016-02-24 15:24:30 +0100

    Pujant canvi 2 Netbeans

--- repoEDD1/src/repoedd1/RepoEDD1.java
+++ repoEDD1/src/repoedd1/RepoEDD1.java
@@ -17,6 +17,7 @@ public class RepoEDD1 {
     public static void main(String[] args) {
         System.out.println ("IDE Netbeans");
         System.out.println("Hola que tal");
+        System.out.println("Més modificacions");
     }
     
 }
